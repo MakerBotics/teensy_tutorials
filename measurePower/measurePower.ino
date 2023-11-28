c605e0eff0435df2550d15328b0f85dba3b08cf4
MakerBotics <137261441+MakerBotics@users.noreply.github.com>
2023-11-28 11:50:38 +1100

    Created measurePower tutorial
    Created a tutorial for a INA226 current sensor to measure and display readings on a teensy4.1

---
+++ measurePower/measurePower.ino
@@ -0,0 +1,34 @@
+/**
+  * Created by Maker Store 28/11/23
+  * Module for reading & displaying the INA226 sensor output using a teensy 4.1
+**/
+
+#include <Wire.h>
+#include "INA226.h"
+
+INA226 INA(0x40);
+
+// Initialising the INA226 sensor
+void setup() {
+  Serial.begin(9600);
+
+  Wire.begin();
+  if (!INA.begin()) { Serial.println("could not connect. Fix and Reboot"); }
+  INA.setMaxCurrentShunt(1, 0.002);
+}
+
+// Polling & displaying the sensor readings every 2 seconds
+void loop() {
+  Serial.print("\nBus Voltage:");
+  Serial.print(INA.getBusVoltage(), 3);
+
+  Serial.print("\tShunt Voltage(mV): ");
+  Serial.print(INA.getShuntVoltage_mV(), 3);
+
+  Serial.print("\tCurrent(mA): ");
+  Serial.print(INA.getCurrent_mA(), 3);
+
+  Serial.print("\tPower(mW): ");
+  Serial.print(INA.getPower_mW(), 3);
+  delay(2000);
+}
